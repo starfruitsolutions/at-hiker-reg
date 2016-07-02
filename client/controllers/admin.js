b14935aef0dcf6017228a0c5e1fbfdaef9d85103
starfruit <administrative@starfruitsolutions.com>
2016-07-02 13:59:45 -0400

    Add table print and regNum range

--- client/controllers/admin.js
+++ client/controllers/admin.js
@@ -9,6 +9,29 @@ Template.AdminHomeLayout.onCreated(function () {
 		self.subscribe('settings');
 		self.subscribe('hikers');
 	});
+	//load the export stuff
+	$.getScript('js/tableExport.js', function(){
+        // script should be loaded and do something with it. 
+
+    });
+	$.getScript('js/jquery.base64.js', function(){
+        // script should be loaded and do something with it. 
+
+    });
+	$.getScript('js/jspdf/libs/sprintf.js', function(){
+        // script should be loaded and do something with it. 
+
+    });
+	$.getScript('js/jspdf/jspdf.js', function(){
+        // script should be loaded and do something with it. 
+
+    });
+	$.getScript('js/jspdf/libs/base64.js', function(){
+        // script should be loaded and do something with it. 
+
+    });
+	Session.set('min','0')
+	
 });
 
 Template.SettingsFormLayout.onCreated(function () {
@@ -25,23 +48,64 @@ Template.AdminHomeLayout.helpers({
 	}
 });
 
-//old table
-Template.RegistrationTableLayout.helpers({	
-	hikers: function () {
-        return Hikers;
-    },
-	fields: ()=> {
-		return [
-	           { key: 'first_name', label: 'First Name' },
-	           { key: 'last_name', label: 'Last Name' },
-	           { key: 'trail_name', label: 'Trail Name' },
-	           { key: 'date', label: 'Date' },
-	           { key: 'regNum', label: 'Registration Number' },
-	           { key: 'timestamp', label: 'Timestamp' },
-               { key: 'delete', label: '', fn: function () { return new Spacebars.SafeString('<button type="button" class="deletebtn">Delete</button>') } }
-	       ]
+//datatable
+dataTableData = function () {
+	if (Session.get('min') && Session.get('max')){ 
+		//filter between 2 numbers
+		var min = Session.get( "min" );
+		var max = Session.get( "max" );
+		console.log('session set');
+		var filter ={ 
+				regNum: { $gte: min, $lte: max }
+		}
+		return Hikers.find(filter).fetch();
 	}
+	else{
+		return Hikers.find().fetch();
+	}
+};
+
+Template.RegistrationTableLayout.helpers({
+	reactiveDataFunction: function () {
+        return dataTableData;
+    },
+    optionsObject: {
+    	columns: [
+    	          { data: 'first_name', title: 'First Name' },
+    	          { data: 'last_name', title: 'Last Name' },
+    	          { data: 'trail_name', title: 'Trail Name' },    	          
+    	          { data: 'regNum', title: 'Registration Number' },
+    	          { data: 'date', title: 'Estimated Arrival' },
+    	          { data: 'timestamp', title: 'Timestamp' },
+    	          { data: '_id', title: 'Delete', render: deleteButton }
+    	        ],
+    	dom: 'lftp', 
+    	// ... see jquery.dataTables docs for more
+    }
 });
+
+function deleteButton(cellData, renderType, currentRow) {
+    // You can return html strings, change sort order etc. here
+    // Again, see jquery.dataTables docs
+	return new Spacebars.SafeString('<button _id='+cellData+' type="button" class="deletebtn">Delete</button>');
+}
+////old table
+//Template.RegistrationTableLayout.helpers({	
+//	hikers: function () {
+//        return Hikers;
+//    },
+//	fields: ()=> {
+//		return [
+//	           { key: 'first_name', label: 'First Name' },
+//	           { key: 'last_name', label: 'Last Name' },
+//	           { key: 'trail_name', label: 'Trail Name' },
+//	           { key: 'date', label: 'Date' },
+//	           { key: 'regNum', label: 'Registration Number' },
+//	           { key: 'timestamp', label: 'Timestamp' },
+//               { key: 'delete', label: '', fn: function () { return new Spacebars.SafeString('<button type="button" class="deletebtn">Delete</button>') } }
+//	       ]
+//	}
+//});
 
 Template.SettingsFormLayout.helpers({
 	publicActiveOptions: ()=> {
@@ -128,26 +192,31 @@ Template.AdminHomeLayout.events({
 	    		},
 	    	});*/
 	  },
-	  'click .reactive-table tbody tr': function (event) {
+	  'change #min': function (event) {
+		  Session.set( "min", parseInt($('#min').val()));
+	  },
+	  'change #max': function (event) {
+		  Session.set( "max", parseInt($('#max').val()));
+	  },
+	  'click .deletebtn': function (event) {
 	    event.preventDefault();
-	    var objToDelete = this;
-	    // checks if the actual clicked element has the class `deletebtn `
-	    if (event.target.className == "deletebtn") {
-		    var poolId = Settings.findOne({name: "RegNumPool"})._id;
-		    var recycleRegNum = Hikers.findOne({_id: objToDelete._id}).regNum;
-			Settings.update(poolId, {
-	    		$addToSet: {
-	    			value: recycleRegNum,
-	    		},
-	    	});
-	        /*var pool = Settings.findOne({name: "RegNumPool"}).value.sort(function(a, b){return a-b});
-	        console.log("Supposed to be sorted: " + pool);
-	    	Settings.update(poolId, {
-	    		$set: {
-	    			value: pool,
-	    		},
-	    	});*/
-	    	Hikers.remove(objToDelete._id);
-	    }
+	    var objToDelete=$(event.currentTarget).attr("_id");	    	  	   
+    	console.log('delete:'+objToDelete);
+	    var poolId = Settings.findOne({name: "RegNumPool"})._id;
+	    var recycleRegNum = Hikers.findOne({_id: objToDelete}).regNum;
+		Settings.update(poolId, {
+    		$addToSet: {
+    			value: recycleRegNum,
+    		},
+    	});
+        /*var pool = Settings.findOne({name: "RegNumPool"}).value.sort(function(a, b){return a-b});
+        console.log("Supposed to be sorted: " + pool);
+    	Settings.update(poolId, {
+    		$set: {
+    			value: pool,
+    		},
+    	});*/
+    	Hikers.remove(objToDelete);
+	    
 	  }
 });
